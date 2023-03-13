ab082708aae6e41d6f659543addde99e311d086e
privet8yaliza <121447016+privet8yaliza@users.noreply.github.com>
2023-03-14 00:49:57 +0200

    Update sf_1.py

--- sf_1.py
+++ sf_1.py
@@ -35,7 +35,7 @@ def convert(message: telebot.types.Message):
 		amount = int(amount)
 
 		if amount < 0:
-			raise ConvertionException("\nСумма конвертации не может принимать отрицательное значение.")
+			raise ConvertionException("\nСумма конвертации не может принимать отрицательное значение.\nПерейти к инструкции по команде: /help")
 			
 		total_base = CryptoConverter.get_price(quote, base, amount)
 
